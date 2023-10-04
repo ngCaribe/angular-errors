597d59e28b78987fbe316a6d6c8e16ab2204cedc
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-10-04 14:54:47 -0600

    Redirrect to errors on load and fix css bug on mobile

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,22 +1,6 @@
-import { RouteMeta } from '@analogjs/router';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RedirectRouteMeta } from '@analogjs/router/lib/models';
 
-export const routeMeta: RouteMeta = {
-  title: 'Angular Errors',
+export const routeMeta: RedirectRouteMeta = {
+  redirectTo: '/errors',
+  pathMatch: 'full',
 };
-
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  template: `
-    <div class="flex pl-16 pt-16">
-      <h1 class="md:text-5xl text-3xl font-bold">Angular Errors</h1>
-    </div>
-  `,
-  host: {
-    class: 'flex flex-1 ',
-  },
-  imports: [RouterLink],
-})
-export default class HomeComponent {}
